c23115f0e176781b595e9dadcfb58bfb1a07ebf9
Kelly Munro <kellymdev@gmail.com>
2015-07-26 16:58:08 +1200

    Added welcome message and error message

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -3,6 +3,10 @@ function ImageController() {
   this.view = new ImageView();
 }
 
+ImageController.prototype.requestWelcomeMessage = function() {
+  this.view.displayWelcomeMessage();
+};
+
 ImageController.prototype.createImageRequest = function(event) {
   this.model.requestImage(event, this.view.displayImage, this.view.displayError);
 };
--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -1,6 +1,14 @@
 function ImageView() {
 }
 
+ImageView.prototype.displayWelcomeMessage = function() {
+  var welcomeHtml = '<div id="welcome-message">' +
+                      '<p>Click on the wall to hang a photo in the gallery.</p>' +
+                      '<p>Click on a photo to view it larger.</p>' +
+                    '</div>';
+  $('#gallery-wall').append(welcomeHtml);
+};
+
 ImageView.prototype.displayImage = function(event, imageDetails) {
   var imageHtml = '<div class="gallery-image">' +
                     '<img src="assets/' + imageDetails.filename + '"">' +
@@ -17,5 +25,11 @@ ImageView.prototype.displayImage = function(event, imageDetails) {
 }
 
 ImageView.prototype.displayError = function() {
-
+  $('#welcome-message').remove();
+  $('.error-message').remove();
+  var errorHtml = '<div class="error-message">' +
+                    '<p>It looks like something went wrong.</p>' +
+                    '<p>Please try again.</p>' +
+                  '</div>';
+  $('#gallery-wall').append(errorHtml);
 }
