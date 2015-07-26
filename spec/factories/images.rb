34ebb776a85f8e616d81863744292238bf459855
Kelly Munro <kellymdev@gmail.com>
2015-07-26 13:11:33 +1200

    Image model and migration

---
+++ spec/factories/images.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :image do
+    title "Summer days"
+    description "The rain in Spain falls mainly on the plains"
+    filename "test.jpg"
+  end
+
+end
---
+++ spec/models/image_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  let!(:image) { create(:image) }
+
+  describe "validations" do
+    it "has a title" do
+      expect(image).to validate_presence_of(:title)
+    end
+
+    it "has a description" do
+      expect(image).to validate_presence_of(:description)
+    end
+
+    it "has a filename" do
+      expect(image).to validate_presence_of(:filename)
+    end
+  end
+
+  after do
+    Image.destroy_all
+  end
+end
