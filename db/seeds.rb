26d974eef8925bd2c8356ff1a13e056406a4e7fa
Kelly Munro <kellymdev@gmail.com>
2016-02-22 14:31:51 +1300

    Added additional images to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,67 @@
 images = Image.create([
+                        # {
+                        #   title: 'Mallard Ducklings',
+                        #   description: 'Mallard Ducklings, Anas platyrhynchos.',
+                        #   filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Ducklings.jpg'
+                        # },
+                        # {
+                        #   title: 'Mandarin Duck',
+                        #   description: 'Mandarin Duck, Aix galericulata.',
+                        #   filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/MandarinDuck.jpg'
+                        # },
+                        # {
+                        #   title: 'Arapawa Island Goat Kid',
+                        #   description: 'Arapawa Island Goat Kid - Capra aegagrus hircus.',
+                        #   filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/GoatKid.jpg'
+                        # },
+                        # {
+                        #   title: 'New Zealand Fur Seal',
+                        #   description: 'New Zealand Fur Seal, Arctocephalus forsteri.',
+                        #   filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Seal.jpg'
+                        # },
+                        # {
+                        #   title: 'North Island Robin',
+                        #   description: 'North Island Robin or Toutouwai, Petroica longipes.',
+                        #   filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/BWRobin.jpg'
+                        # },
+                        # {
+                        #   title: 'Mount Ruapehu',
+                        #   description: 'Mount Ruapehu, Tongariro National Park.',
+                        #   filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Ruapehu.jpg'
+                        # }
                         {
-                          title: 'Mallard Ducklings',
-                          description: 'Mallard Ducklings, Anas platyrhynchos.',
-                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Ducklings.jpg'
+                          title: 'California Quail',
+                          description: 'California Quail, Callipepla californica.',
+                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/CaliforniaQuail.jpg'
                         },
                         {
-                          title: 'Mandarin Duck',
-                          description: 'Mandarin Duck, Aix galericulata.',
-                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/MandarinDuck.jpg'
+                          title: 'Pied Stilt',
+                          description: 'Pied Stilt, Himantopus himantopus',
+                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/PiedStiltReflection.jpg'
                         },
                         {
-                          title: 'Arapawa Island Goat Kid',
-                          description: 'Arapawa Island Goat Kid - Capra aegagrus hircus.',
-                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/GoatKid.jpg'
+                          title: 'Pied Stilt',
+                          description: 'Pied Stilt, Himantopus himantopus',
+                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/PiedStiltStanding.jpg'
                         },
                         {
-                          title: 'New Zealand Fur Seal',
-                          description: 'New Zealand Fur Seal, Arctocephalus forsteri.',
-                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Seal.jpg'
+                          title: 'Kereru',
+                          description: 'New Zealand Pigeon (Kereru), Hemiphaga novaeseelandiae',
+                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Kereru.jpg'
                         },
                         {
-                          title: 'North Island Robin',
-                          description: 'North Island Robin or Toutouwai, Petroica longipes.',
-                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/BWRobin.jpg'
+                          title: 'Takahe',
+                          description: 'Takahe, Porphyrio hochstetteri',
+                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Takahe.jpg'
                         },
                         {
-                          title: 'Mount Ruapehu',
-                          description: 'Mount Ruapehu, Tongariro National Park.',
-                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Ruapehu.jpg'
+                          title: 'Kakariki',
+                          description: 'Red-crowned Parakeet (Kakariki), Cyanoramphus novaezelandiae',
+                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/Kakariki.jpg'
+                        },
+                        {
+                          title: 'New Zealand Scaup Duckling',
+                          description: 'New Zealand Scaup Duckling, Aythya novaeseelandiae',
+                          filename: 'https://s3-ap-southeast-2.amazonaws.com/photographysite/ScaupDuckling.jpg'
                         }
                       ])
