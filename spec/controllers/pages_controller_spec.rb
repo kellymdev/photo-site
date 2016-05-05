64dc626164c6aa80e4f6792f22daa12906ba9378
Kelly Munro <kellymdev@gmail.com>
2016-05-05 14:25:19 +1200

    Remove homepage tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,18 +3,6 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
   render_views
 
-  describe 'get #home' do
-    before { get :home }
-
-    it 'returns http status 200' do
-      expect(response.status).to eq(200)
-    end
-
-    it 'renders the home template' do
-      expect(response).to render_template(:home)
-    end
-  end
-
   describe 'get #about' do
     before { get :about }
 
