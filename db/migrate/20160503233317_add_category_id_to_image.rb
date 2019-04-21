919e945aaf5540bc7db26f84f2f4c2af7e85ef0e
Kelly Munro <kellymdev@gmail.com>
2019-04-21 15:47:10 +1200

    Update migrations to reference Rails 4.2

--- db/migrate/20160503233317_add_category_id_to_image.rb
+++ db/migrate/20160503233317_add_category_id_to_image.rb
@@ -1,4 +1,4 @@
-class AddCategoryIdToImage < ActiveRecord::Migration
+class AddCategoryIdToImage < ActiveRecord::Migration[4.2]
   def change
     add_column :images, :category_id, :integer, foreign_key: true
   end
--- db/migrate/20160504002550_add_image_url_to_category.rb
+++ db/migrate/20160504002550_add_image_url_to_category.rb
@@ -1,4 +1,4 @@
-class AddImageUrlToCategory < ActiveRecord::Migration
+class AddImageUrlToCategory < ActiveRecord::Migration[4.2]
   def change
     add_column :categories, :image_url, :string
   end
